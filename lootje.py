3ba55a0b847d377b28866c5882b1e3c0b7845084
Jelmer Mulder <jmulder14.jm@gmail.com>
2019-11-14 23:17:10 +0100

    Add email

--- lootje.py
+++ lootje.py
@@ -1,23 +1,44 @@
 import random
+import smtplib
+
+gmail_user = '<naam@gmail.com'
+gmail_password = '<wachtwoord>'
 def picker(range):
     pick = random.randint(0, len(range)-1)
     return pick
 
-persoon = ["iris", "katie", "frank", "wouter", "jelmer", "frans", "robin", "bobbie", "nicole", "jos", "frenk", "loel", "martijn", "roos"]
-kiest = ["iris", "katie", "frank", "wouter", "jelmer", "frans", "robin", "bobbie", "nicole", "jos", "frenk", "loel", "martijn", "roos"]
-
-koppelingen = []
-algestoken = []
+persoon = [<lijst_van_personen>] # zelfde volgorde
+kiest = [<lijst_van_personen>] # zelfde volgorde
+emails = [<lijst_van_mail_adres>] # zelfde volgorde
 while persoon != []:
     persoon1 = persoon[0]
+    to = emails[0]
     file = persoon1 + ".txt"
     koppel = kiest[picker(kiest)]
     persoon.remove(persoon1)
     kiest.remove(koppel)
-    with open(file, 'w') as filetowrite:
-        koppelkoppel = persoon1 + " trok " + koppel
-        koppelingen.append(koppelkoppel)
-        algestoken.append(koppel)
-        filetowrite.write(koppelkoppel)
-    
-#print(koppelingen)
+    emails.remove(to)
+    koppelkoppel = persoon1 + " trok " + koppel
+    sent_from = gmail_user
+    subject = 'OMG Super Important Message'
+    body = koppelkoppel
+
+    email_text = """
+    From: %s
+    To: %s
+    Subject: %s
+
+    %s
+    """ % (sent_from, ", ".join(to), subject, body)
+
+    try:
+        server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+        server.ehlo()
+        server.login(gmail_user, gmail_password)
+        server.sendmail(sent_from, to, email_text)
+        server.close()
+
+        print('Email sent!')
+    except:
+        print('Something went wrong...')
+        
